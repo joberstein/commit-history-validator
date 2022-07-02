3d73ebdfd73344c1874ab509e6f4f0da3e4384cf
Jesse Oberstein <7157500+joberstein@users.noreply.github.com>
2022-07-01 22:57:48 -0400

    fix(tests): Fix commit-msg test.
    Issue 13.

--- test/scripts/commit-msg.test.js
+++ test/scripts/commit-msg.test.js
@@ -1,25 +1,32 @@
-import run from "src/scripts/commitMsg";
+import run from "src/scripts/commit-msg";
 import * as commitUtils from "src/utils/commit.js";
 
 describe('src/hooks/commit-msg', () => {
     const validateCurrentCommit = jest.spyOn(commitUtils, 'validateCurrentCommit')
         .mockResolvedValue({});
 
+    const exit = jest.spyOn(process, 'exit')
+        .mockReturnValue({});
+
     it('Exits normally when the current commit message is valid', () => {
-        expect(() => run('filepath')).not.toThrowError();
+        run([ 'filepath' ])
+
         expect(validateCurrentCommit).toHaveBeenCalledWith('filepath');
+        expect(exit).not.toHaveBeenCalled();
     });
 
     it('Errors out when the current commit message is not valid', () => {
-        const errorText = 'Failed';
-        validateCurrentCommit.mockImplementation(() => { throw new Error(errorText); });
-
-        expect(() => run('filepath')).toThrowError(errorText);
+        validateCurrentCommit.mockImplementation(() => { throw new Error('Failed'); });
+        run([ 'filepath' ]);
+        
         expect(validateCurrentCommit).toHaveBeenCalledWith('filepath');
+        expect(exit).toHaveBeenCalledWith(1);
     });
 
     it('Errors out when no commit message path is passed', () => {
-        run();
+        run([]);
+
         expect(validateCurrentCommit).not.toHaveBeenCalled();
+        expect(exit).not.toHaveBeenCalled();
     });
 });

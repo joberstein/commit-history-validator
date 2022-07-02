4b2244dca92feb8bfabfb1746208b333492d5210
Jesse Oberstein <7157500+joberstein@users.noreply.github.com>
2022-07-01 23:15:03 -0400

    chore(dependencies): Remove zx dependency.
    Issue 13.

--- src/utils/exec.js
+++ src/utils/exec.js
@@ -3,7 +3,3 @@ import {execSync} from "child_process";
 export const node = (executable, options) => {
     execSync(`node ${executable} ${options}`);
 }
-
-export const zx = (executable, options) => {
-    execSync(`zx ${executable} ${options}`);
-}

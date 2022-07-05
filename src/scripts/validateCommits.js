f3cd0b6a3a4477cc55e4ff869d2ff9c6a5beb4bb
Jesse Oberstein <7157500+joberstein@users.noreply.github.com>
2022-07-05 00:18:58 -0400

    chore(tests): Add tests for validating commits.
    Issue 13.

--- src/scripts/validateCommits.js
+++ src/scripts/validateCommits.js
@@ -5,7 +5,7 @@ import {getExecutingProjectDirectory} from "../utils/path.js";
 export default (branch) => {
     if (!branch || branch === "master" || !isRemoteBranch(branch)) {
         console.info(`Skipped commit validation on branch '${branch}'.`);
-        process.exit(0);
+        return;
     }
 
     console.info("Fetching commits available on the master branch...");
@@ -15,18 +15,15 @@ export default (branch) => {
 
     if (!from) {
         console.info('All commit messages on this branch have already been validated.');
-        process.exit(0);
+        return;
     }
 
     console.info(`Validating commit messages on branch '${branch}'...\n`);
+
     const projectDir = getExecutingProjectDirectory();
     const config = `${projectDir}/commitlint.config.js`;
 
-    try {
-        validateCommits({from, config});
-    } catch (e) {
-        process.exit(1);
-    }
+    validateCommits({from, config});
 
     console.info('All commit messages successfully validated.');
 }

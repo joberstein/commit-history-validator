70a431b847ac1470ddabe2ffe94295f7bee1f6e4
Jesse Oberstein <7157500+joberstein@users.noreply.github.com>
2022-07-06 00:28:49 -0400

    chore(tests): Update the commit msg hook test.
    Issue 13.

--- test/scripts/commit-msg.test.js
+++ test/scripts/commit-msg.test.js
@@ -2,20 +2,18 @@ import run from "src/scripts/commit-msg";
 import * as commitUtils from "src/utils/commit.js";
 
 describe('scripts/commit-msg', () => {
-    const validateCurrentCommit = jest.spyOn(commitUtils, 'validateCurrentCommit')
-        .mockResolvedValue({});
+    const validateCurrentCommit = jest.spyOn(commitUtils, 'validateCurrentCommit');
 
-    const exit = jest.spyOn(process, 'exit')
-        .mockReturnValue({});
+    beforeEach(() => {
+        validateCurrentCommit.mockResolvedValue({});
+    })
 
     it("Does nothing for a branch that doesn't match the expected format", () => {
-        run([ 'filepath' ]);
-
+        expect(() => run([ 'filepath' ])).not.toThrow();
         expect(validateCurrentCommit).toHaveBeenCalledWith('filepath');
-        expect(exit).not.toHaveBeenCalled();
     });
 
-    it('Errors out when the current commit message is not valid', () => {
+    it('Exits when the current commit message is not valid', () => {
         validateCurrentCommit.mockImplementation(() => { throw new Error('Failed'); });
 
         expect(() => run([ 'filepath' ])).toThrow();
